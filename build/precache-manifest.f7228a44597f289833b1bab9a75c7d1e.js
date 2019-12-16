dcc49722219ecb2f186c1099eae2f332eaf1623e
sharonmalio <kaninimalio@gmail.com>
2019-12-16 11:59:44 -0500

    production build

---
+++ build/precache-manifest.f7228a44597f289833b1bab9a75c7d1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2884150995fee4006c9bc29ea1bac74b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d7cb1a7c2b25491ff69",
+    "url": "/static/css/main.131d7c7e.chunk.css"
+  },
+  {
+    "revision": "0fb245a073daf204c4cf",
+    "url": "/static/js/2.934da36f.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.934da36f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d7cb1a7c2b25491ff69",
+    "url": "/static/js/main.4535396b.chunk.js"
+  },
+  {
+    "revision": "265d6d3fc2f3f6d9fe49",
+    "url": "/static/js/runtime-main.f4c4d351.js"
+  },
+  {
+    "revision": "9b0e4bb3371ccbc5deccbbb9c2afb021",
+    "url": "/static/media/images.9b0e4bb3.jpeg"
+  }
+]);
